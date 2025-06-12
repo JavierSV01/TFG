120dc3440c107142ddc7a87cc81991bfd0e9b6eb
Javier Sabio <fj.sabio@horizontes-informatica.com>
2025-06-12 12:27:47 +0200

    Solucion sockert io

--- backendAppEntrenamientoSocial/run.py
+++ backendAppEntrenamientoSocial/run.py
@@ -1,23 +1,24 @@
 import eventlet
-eventlet.monkey_patch()
+eventlet.monkey_patch() # Asegúrate de que esto esté SIEMPRE en la primera línea ejecutable
 
-from app import create_app, socketio
-from dotenv import load_dotenv
+from app import create_app, socketio # Asumo que 'create_app' crea la instancia de Flask y 'socketio' está ya asociada a ella.
 from app.config import Config
-from flask import Flask
+from flask import jsonify # Importamos jsonify para el health check
 from flasgger import Swagger
-import os
-
-load_dotenv()
-hostEnv = os.getenv('BACKEND_HOST')
-portEnv = os.getenv('BACKEND_PORT')
 
 app = create_app(Config)
 swagger = Swagger(app)
 
+@app.route('/')
+def health_check():
+    """
+    Ruta simple para el chequeo de salud de Render.
+    """
+    return jsonify({"status": "healthy", "message": "Backend is running!"}), 200
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
-    host = '0.0.0.0'
-    socketio.run(app, host=hostEnv, port=portEnv, debug=True)
-    #app.run(host=hostEnv, port=portEnv)
+    host = '0.0.0.0' 
 
+    print(f"Running Flask app with Socket.IO locally on {host}:{port}")
+    socketio.run(app, host=host, port=port, debug=True)
